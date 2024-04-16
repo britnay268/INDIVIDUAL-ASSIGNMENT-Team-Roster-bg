daa9c1fb5c5fb77f36b1cc05b4d820b9159a4f5e
Britnay Gore <britnaygore268@gmail.com>
2024-04-15 21:15:50 -0400

    Create component for member cards and show cards on webpage

--- pages/team.js
+++ pages/team.js
@@ -1,9 +1,27 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
+import { getMember } from '../api/memberData';
+import { useAuth } from '../utils/context/authContext';
+import MemberCard from '../components/MemberCard';
 
 export default function Team() {
+  const [members, setMembers] = useState([]);
+
+  const { user } = useAuth();
+
+  const getAllMembers = () => {
+    getMember(user.uid).then(setMembers);
+  };
+
+  useEffect(() => {
+    getAllMembers();
+  }, []);
+
   return (
-    <div>
-      <h1>Team</h1>
+    <div className="d-flex flex-wrap">
+      {members.map((member) => (
+        <MemberCard key={member.firebaseKey} memberObj={member} onUpdate={getAllMembers} />
+      ))}
     </div>
   );
 }
