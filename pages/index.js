b14f8b46cca23d819313fa0d9962e10f319a3644
Britnay Gore <britnaygore268@gmail.com>
2024-04-15 20:12:50 -0400

    Changed Navbar name and show Team View when logged in

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
-import { Button } from 'react-bootstrap';
-import { signOut } from '../utils/auth';
-import { useAuth } from '../utils/context/authContext';
-
 function Home() {
-  const { user } = useAuth();
+  // const { user } = useAuth();
 
   return (
     <div
@@ -15,11 +11,7 @@ function Home() {
         margin: '0 auto',
       }}
     >
-      <h1>Hello {user.displayName}! </h1>
-      <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
-      </Button>
+      <h1>Team View</h1>
     </div>
   );
 }
